a70b02814847e14074c3f69afcf73e7476977380
Amankos Ansar <zugzwanggg0@gmail.com>
2024-01-26 23:50:11 +0500

    swiper

--- frontend/src/pages/Home/sections/OurMission.tsx
+++ frontend/src/pages/Home/sections/OurMission.tsx
@@ -18,7 +18,7 @@ const OurMission = () => {
           <path id="Polygon 2" d="M39 43L0.894886 0.25L77.1051 0.249993L39 43Z" fill="#735CFF"/>
         </svg>
 
-        <svg className="hidden md:block md:absolute left-0 bottom-0" xmlns="http://www.w3.org/2000/svg" width="820" height="110" viewBox="0 0 820 110" fill="none">
+        <svg className="hidden md:block md:absolute left-0 -bottom-2" xmlns="http://www.w3.org/2000/svg" width="820" height="110" viewBox="0 0 820 110" fill="none">
           <path d="M694.166 0H-157V110H820L694.166 0Z" fill="white"/>
         </svg>
       </div>
