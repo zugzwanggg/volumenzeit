3dba72764a5e140289d6fedfbc1c2d8f553e9203
Amankos Ansar <zugzwanggg0@gmail.com>
2024-02-02 17:49:08 +0500

    prettier

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -6,7 +6,9 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        'plugin:react-hooks/recommended',
+        'prettier',
     ],
     "overrides": [
         {
